bef4e2aa8dde899bcb254a0bde2882b85c3f9e7e
DaCosta Smith <dacostasmith@macs-MacBook-Air.local>
2019-08-04 15:39:40 -0400

    index

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 //___________________
 //localhost:3000
 app.get('/' , (req, res) => {
-  res.send('Hello World! man');
+  res.render('index.ejs');
 });
 
 //___________________
